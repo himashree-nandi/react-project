e5523b73ca7814901a4badb61d214a7329a69082
Himashree Nandi <nandihimashree@gmail.com>
2023-01-07 11:04:21 +0530

    modified

--- src/component/Component1.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Component1() {
-  return (
-    <div> <h1>this is Component1</h1></div>
-  )
-}
--- src/component/Component2.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Component2() {
-  return (
-    <div> <h1>this is Component2</h1></div>
-  )
-}
